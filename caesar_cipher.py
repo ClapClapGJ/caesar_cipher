53ccbaff137a22c2be061a5257889c97ef89c7fd
Artem <fantazerbeda@gmail.com>
2022-11-20 19:40:51 +0400

    remove magic numbers, fixed uppercase

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,15 +1,21 @@
-message = 'Hello, world'
+message = 'Hello, World'
 shift = 7
+
+LAST_CHAR_CODE = 90
+CHAR_RANGE = 26
 
 # Result placeholder
 result = ''
 
-for char in message:
+for char in message.upper():
     if char.isalpha():
         # Convert character to the ASCII code
         char_code = ord(char)
 
         new_char_code = char_code+shift
+
+        if new_char_code > LAST_CHAR_CODE:
+            new_char_code -= CHAR_RANGE
 
         new_char = chr(new_char_code)
         result += new_char
